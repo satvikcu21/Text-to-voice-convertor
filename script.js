060885a8919e270f2c7b123c4ae78745abc92df2
Satvik kushwaha <150938638+satvikcu21@users.noreply.github.com>
2024-06-25 19:22:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let speech=new SpeechSynthesisUtterance();
+document.querySelector("button").addEventListener("click",()=> {
+    speech.text=document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+})
